fd969c81424f0b9c3a4743c0bf5c65d6d088d09f
rangambr <rangambr@gmail.com>
2014-08-31 16:08:00 +0100

    changes

--- admin/controller/respond_inquiry.php
+++ admin/controller/respond_inquiry.php
@@ -9,7 +9,7 @@ $email=$_POST['txt_to'];
 $inquiry_obj=new Admin();
 $inquiry_obj->insertRespond($res);
 
-$inquiry_obj->emailRespond($email,$sub,$res)
+$inquiry_obj->emailRespond($email,$sub,$res);
 //
 //$user->activation_email($email,"Activate your account","Hello ".$username.", \n\n You need to activate your account , Please use the link below: \n\n http://localhost/GVPD/home/view/activate_user.php?ecode=$email_code&username=$username \n\n--Green Valley Property Development (PVT) Ltd--");
 //	
